2bc08a78476f5616c010f135595764b80eea2764
iorid3 <itakeshita@my.bcit.ca>
2022-07-31 20:19:08 -0700

    cors

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/user')
 const todoRouter = require('./routes/todo')
 const config = require('./config')
-//  const cors = require('cors');
+const cors = require('cors');
 
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect(config.MONGOODB_URL, (err) => {
   if(err) return console.log(err)
   console.log('connected to db successfully')
 })
+
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   next();
