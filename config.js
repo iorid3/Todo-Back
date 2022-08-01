fa8fc8ea9235cfbca29610b8b759665e35f91759
iorid3 <itakeshita@my.bcit.ca>
2022-07-31 23:14:09 -0700

    config

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    MONGOODB_URL: 'mongodb+srv://ioritakeshita:FEuPKGxQ73kJU7EV@cluster0.ba9ch.mongodb.net/?retryWrites=true&w=majority',
+    MONGOODB_URL: 'mongodb+srv://ioritakeshita:jenkeOdPyvbRz7dc@cluster0.ba9ch.mongodb.net/?retryWrites=true&w=majority',
     SECRET: 'thisismysecret'
   }
+
+
